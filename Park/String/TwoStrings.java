04cc57bbe56c6f87bd147c318edd680b807fd4b8
joohee.park <joohee.park@nhnent.com>
2020-02-11 18:41:56 +0900

    change file names

--- Park/String/TwoStrings.java
+++ Park/String/TwoStrings.java
@@ -13,7 +13,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Solution {
+public class TwoStrings {
 
     // Complete the alternate function below.
     static int alternate(String str) {
